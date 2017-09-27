0a43d5b3301d9fca7ca07cb671134729f51b8639
DmitriC123 <cohan.dima@gmail.com>
2017-09-27 13:38:57 -0400

    Practice with age if-else

--- Week1_Day3/Program.cs
+++ Week1_Day3/Program.cs
@@ -18,27 +18,58 @@ namespace Week1_Day3
             //    //Do something
             //    Console.WriteLine("You have broken math. Good job.");
             //}
+
             //else
             //{
             //    //Do something else
             //    Console.WriteLine("That makes sense.");
             //}
 
-            Console.WriteLine("What day of the week is it?");
-            string day = Console.ReadLine();
+            //Console.WriteLine("What day of the week is it?");
+            //string day = Console.ReadLine();
 
-            if (day == "Monday")
-            {
-                Console.WriteLine("Back to work.");
-            }
-            else if (day == "Friday")
-            {
-                Console.WriteLine("TGIF");
-            }
-            else
-            {
-                Console.WriteLine("Bloop");
-            }
+            //if (day == "Monday")
+            //{
+            //    Console.WriteLine("Back to work.");
+            //}
+            //else if (day == "Friday")
+            //{
+            //    Console.WriteLine("TGIF");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Bloop");
+            //}
+
+            //Console.WriteLine("How old are you?");
+            //int userAge = int.Parse(Console.ReadLine());
+
+            //if (userAge >= 0 && userAge <= 1)
+            //{
+            //    Console.WriteLine("You're just a baby!");
+            //}
+            //else if (userAge > 1 && userAge < 4)
+            //{
+            //    Console.WriteLine("Yay toddlers!");
+            //}
+            //else if (userAge >= 4 && userAge < 13)
+            //{
+            //    Console.WriteLine("Enjoy your childhood.");
+            //}
+            //else if (userAge >= 13 && userAge < 65)
+            //{
+            //    Console.WriteLine("Life is akward");
+            //}
+            //else if (userAge >= 65 && userAge < 100)
+            //{
+            //    Console.WriteLine("Enjoy your retirement");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("That's not really your age.");
+            //}
+
+
         }
     }
 }
